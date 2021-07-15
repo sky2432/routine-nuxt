bdd0147ab15c5ef307aa98f71e469629df2c79d3
sky2432 <skyarts0122@gmail.com>
2021-07-15 10:47:20 +0900

    ✨Add: トップページ追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,77 +1,43 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <v-app-bar fixed app>
+      <v-toolbar-title>RoutineQuest</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <NuxtLink to="/other" class="mr-4">サインアップ</NuxtLink>
+
+      <NuxtLink to="/other">ログイン</NuxtLink>
+    </v-app-bar>
+    <v-main class="main">
+      <div class="top-message">
+        <h1>ゲームのように習慣を身につけよう</h1>
+        <h1>習慣が人生を作る</h1>
+        <v-btn class="mt-4 top-btn" large>はじめる</v-btn>
+      </div>
+    </v-main>
+    <v-footer>
+      <a href="https://storyset.com/marketing"
+        >Marketing illustrations by Storyset</a
+      >
+    </v-footer>
+  </div>
 </template>
+
+<style scoped>
+.main {
+  height: 100vh;
+  background-image: url('@/static/top-page-image.png');
+  background-size: cover;
+  background-position: 0px 64px;
+  background-repeat: no-repeat;
+}
+
+.top-message {
+  margin-top: 7%;
+  margin-left: 5%;
+}
+
+.top-btn {
+  margin-left: 13%;
+}
+</style>

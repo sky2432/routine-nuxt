44a9741e49cc66fcd823d6da0b713c214de1f407
sky2432 <skyarts0122@gmail.com>
2021-08-07 12:34:19 +0900

    ✨Add: アカウント削除完了ページ追加

---
+++ src/pages/deleted.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-main>
+    <v-container>
+      <div class="wrapper">
+        <v-card width="500" tile>
+          <v-card-title class="justify-center">Deleted</v-card-title>
+          <v-card-text class="text-center">
+            アカウントは削除されました<br>
+            またのご利用お待ちしております
+          </v-card-text>
+          <v-card-actions class="justify-center">
+            <v-btn @click="$router.push('/')">トップページへ</v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </v-container>
+  </v-main>
+</template>

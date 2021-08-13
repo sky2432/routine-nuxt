851bb87f40a339fd22e861790a0c5f8126e01c47
sky2432 <skyarts0122@gmail.com>
2021-08-13 12:20:56 +0900

    ✅Test: トップページテスト追加

---
+++ src/test/Index.spec.ts
@@ -0,0 +1,27 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import index from '../pages/index.vue'
+import Vuetify from 'vuetify'
+
+describe('index page', () => {
+  const localVue = createLocalVue()
+  let vuetify: Vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  const mountFunction = (options?: any) => {
+    return mount(index, {
+      localVue,
+      vuetify,
+      ...options,
+    })
+  }
+
+  test('is a text', () => {
+    const wrapper = mountFunction()
+    expect(wrapper.text()).toBe(
+      'RoutineQuest  サインアップ ログイン ゲームのように習慣を身につけよう 習慣が人生を作る 試してみる Marketing illustrations by Storyset'
+    )
+  })
+})
